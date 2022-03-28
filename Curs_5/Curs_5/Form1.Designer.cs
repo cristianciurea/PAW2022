d5a506b7a140459df7d3285373513c6db9b32264
CRISTIAN CIUREA <cristianciurea@gmail.com>
2022-03-28 15:22:47 +0300

    update

---
+++ Curs_5/Curs_5/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace Curs_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.schimbaGrosimeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.schimbaCuloareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvareImagineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.contextMenuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 460);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.LargeChange = 1;
+            this.hScrollBar1.Location = new System.Drawing.Point(13, 13);
+            this.hScrollBar1.Maximum = 5;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(80, 17);
+            this.hScrollBar1.TabIndex = 1;
+            this.hScrollBar1.Value = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.schimbaGrosimeToolStripMenuItem,
+            this.schimbaCuloareToolStripMenuItem,
+            this.salvareImagineToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(167, 70);
+            // 
+            // schimbaGrosimeToolStripMenuItem
+            // 
+            this.schimbaGrosimeToolStripMenuItem.Name = "schimbaGrosimeToolStripMenuItem";
+            this.schimbaGrosimeToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.schimbaGrosimeToolStripMenuItem.Text = "Schimba grosime";
+            this.schimbaGrosimeToolStripMenuItem.Click += new System.EventHandler(this.schimbaGrosimeToolStripMenuItem_Click);
+            // 
+            // schimbaCuloareToolStripMenuItem
+            // 
+            this.schimbaCuloareToolStripMenuItem.Name = "schimbaCuloareToolStripMenuItem";
+            this.schimbaCuloareToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.schimbaCuloareToolStripMenuItem.Text = "Schimba culoare";
+            this.schimbaCuloareToolStripMenuItem.Click += new System.EventHandler(this.schimbaCuloareToolStripMenuItem_Click);
+            // 
+            // salvareImagineToolStripMenuItem
+            // 
+            this.salvareImagineToolStripMenuItem.Name = "salvareImagineToolStripMenuItem";
+            this.salvareImagineToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.salvareImagineToolStripMenuItem.Text = "Salvare imagine";
+            this.salvareImagineToolStripMenuItem.Click += new System.EventHandler(this.salvareImagineToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.ContextMenuStrip = this.contextMenuStrip1;
+            this.panel1.Controls.Add(this.hScrollBar1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1025, 485);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1025, 485);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem schimbaGrosimeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem schimbaCuloareToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvareImagineToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Curs_5/Curs_5/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Curs_5
+{
+    public partial class Form1 : Form
+    {
+        ArrayList listaPct;
+        bool mouseApasat;
+        Graphics context;
+        Bitmap img;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            listaPct = new ArrayList();
+            //context = this.CreateGraphics();
+            //img = new Bitmap(this.Width, this.Height);
+            img = new Bitmap(panel1.Width, panel1.Height);
+            context = Graphics.FromImage(img);
+            hScrollBar1.Visible = false;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                Point p = new Point(e.X, e.Y);
+                listaPct.Add(p);
+                mouseApasat = true;
+                label1.Text = "Form1_MouseDown";
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button==MouseButtons.Left)
+            {
+                Point p = new Point(e.X, e.Y);
+                listaPct.Add(p);
+                Pen creion = new Pen(this.ForeColor);
+                creion.Width = hScrollBar1.Value;
+                if(mouseApasat==true)
+                {
+                    context.DrawLine(creion, (Point)listaPct[listaPct.Count - 2], (Point)listaPct[listaPct.Count - 1]);
+                    label1.Text = "Form1_MouseMove";
+                    //Invalidate();
+                    panel1.Invalidate();
+                }
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseApasat = false;
+                label1.Text = "Form1_MouseUp";
+            }
+        }
+
+        private void schimbaGrosimeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            hScrollBar1.Visible = true;
+        }
+
+        private void schimbaCuloareToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog dlg = new ColorDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+                this.ForeColor = dlg.Color;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(img, 0, 0);
+        }
+
+        private void salvare(Control c, string denumire)
+        {
+            Graphics g = c.CreateGraphics();
+            Bitmap bmp = new Bitmap(c.Width, c.Height);
+            c.DrawToBitmap(bmp, new Rectangle(c.ClientRectangle.X, c.ClientRectangle.Y,
+                c.Width, c.Height));
+            bmp.Save(denumire);
+            bmp.Dispose();
+        }
+
+        private void salvareImagineToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            salvare(panel1, "desen.bmp");
+            MessageBox.Show("S-a salvat!");
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(img, 0, 0);
+        }
+    }
+}
